c03c330dbfb16f52c473ac9a74456af5161e8e59
Dominick Simone <domsimone123@gmail.com>
2022-03-11 16:04:30 -0500

    adding routes

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -3,12 +3,20 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './css/App.css';
 import Home from "./pages/Home"
+import Menu from "./pages/Menu"
+import Order from "./pages/Order"
 
 function App() {
   return (
     <Router>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/order">
+        <Order />
+      </Route>
+      <Route exact path="/menu">
+        <Menu />
       </Route>
     </Router>
   );
